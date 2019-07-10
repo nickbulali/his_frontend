cec9a7c9619cb3d9ba47eacd31efb17a849fb56e
fredrick muiruri <Nyati_1@Nyati01.strathmore.local>
2019-07-10 11:40:40 +0300

    show invoice

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
