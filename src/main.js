652cf33739039de2356def706f10db66904e4a76
ereng <aereng@gmail.com>
2019-10-18 10:31:56 +0300

    Turns out: environment variables MUST be specified with prefix "VUE_APP_" 😒

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+
 import dotenv from 'dotenv'
 dotenv.config()
+console.log(process.env)
 
 Vue.config.productionTip = false
 new Vue({
